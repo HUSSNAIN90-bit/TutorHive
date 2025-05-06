b940977465e9042889d7f428fa92f6d8c58d5afc
HUSSNAIN90-bit <hussnainsalar38@gmail.com>
2025-05-06 05:19:13 -0700

    Section 2 was completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <section className="flex pt-[120px] px-10 h-screen">
+      <section className="flex pt-[120px] px-10 h-screen ">
         <div className="flex flex-col gap-8 justify-center w-[50%] p-2">
           <div className="flex gap-3 items-center w-fit  px-10 py-2 bg-[#FAC43C99] rounded-4xl">
             <div className="max-w-[100px]">
@@ -38,7 +38,7 @@ const Home = () => {
           />
         </div>
       </section>
-      <section className="flex p-10 flex-col">
+      <section className="flex p-10 flex-col justify-center">
         <div className="flex flex-col gap-5 items-center w-full">
           <div className="bg-[#FAC43C99] px-10 py-3 rounded-4xl">
             <p className="text tracking-wider">WHY CHOOSE US</p>
@@ -52,8 +52,87 @@ const Home = () => {
             Dive into online courses on diverse subjects
           </h2>
         </div>
-        <div className="w-full flex gap-10 justify-center items-center flex-wrap">
-          
+        <div className="w-full flex justify-center gap-[50px] p-10 flex-wrap">
+          <div className="flex flex-col gap-[50px]">
+            <div className="w-[650px] bg-white rounded-2xl flex p-2" style={{
+              boxShadow:" rgb(218, 218, 218) 0px 4px 5px 2px"
+            }}>
+              <div className="h-full p-4">
+                <div className="p-8 bg-[#FAC43C] rounded-2xl">
+                  <img src="/images/Vector.png" alt=""className="w-40"  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-3 p-4">
+                <p className="normal-heading font-[600]">
+                  Progress Tracking and Certifications
+                </p>
+                <p className="text-2">
+                  Progress tracking and certifications provide learners with
+                  clear milestones and recognition of their achievements. By
+                  monitoring progress, students can stay motivated.
+                </p>
+              </div>
+            </div>
+            <div className="w-[650px] bg-white rounded-2xl flex p-2" style={{
+              boxShadow:" rgb(218, 218, 218) 0px 4px 5px 2px"
+            }}>
+              <div className="h-full p-4">
+                <div className="p-8 bg-[#FF8345] rounded-2xl">
+                  <img src="/images/Vector.png" alt=""className="w-40"  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-3 p-4">
+                <p className="normal-heading font-[600]">
+                  Progress Tracking and Certifications
+                </p>
+                <p className="text-2">
+                  Progress tracking and certifications provide learners with
+                  clear milestones and recognition of their achievements. By
+                  monitoring progress, students can stay motivated.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[50px]">
+            <div className="w-[650px] bg-white rounded-2xl flex p-2" style={{
+              boxShadow:" rgb(218, 218, 218) 0px 4px 5px 2px"
+            }}>
+              <div className="h-full p-4">
+                <div className="p-8 bg-[#0066B4] rounded-2xl">
+                  <img src="/images/Vector.png" alt=""className="w-40"  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-3 p-4">
+                <p className="normal-heading font-[600]">
+                  Progress Tracking and Certifications
+                </p>
+                <p className="text-2">
+                  Progress tracking and certifications provide learners with
+                  clear milestones and recognition of their achievements. By
+                  monitoring progress, students can stay motivated.
+                </p>
+              </div>
+            </div>
+            <div className="w-[650px] bg-white rounded-2xl flex p-2" style={{
+              boxShadow:" rgb(218, 218, 218) 0px 4px 5px 2px"
+            }}>
+              <div className="h-full p-4">
+                <div className="p-8 bg-[#48B774] rounded-2xl">
+                  <img src="/images/Vector.png" alt=""className="w-40"  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-3 p-4">
+                <p className="normal-heading font-[600]">
+                  Progress Tracking and Certifications
+                </p>
+                <p className="text-2">
+                  Progress tracking and certifications provide learners with
+                  clear milestones and recognition of their achievements. By
+                  monitoring progress, students can stay motivated.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
